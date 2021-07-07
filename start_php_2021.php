47efc352e5fc96b50b469f841925049865c15244
KiUkKim <zidh1@korea.ac.kr>
2021-07-07 22:46:49 +0900

    php start

---
+++ start_php_2021.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang = "ko">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?
+      echo "반갑습니다.<br>";
+      echo "행복한 하루 되세요!";
+    ?>
+  </body>
+</html>
