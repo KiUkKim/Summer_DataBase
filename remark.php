dd7f244855004ef4bec7511d44d61a54a63eeaee
KiUkKim <zidh1@korea.ac.kr>
2021-07-07 22:54:05 +0900

    php 실습

---
+++ remark.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?
+      // [예제 2-2]는 주석문을 설명하는 프로그램이다. 이 행은 주석 처리된다.
+      /* 여러 행을 주석 처리할 때는 이와 같이 한다. */
+
+      echo "이 문장은 출력된다.";
+    ?>
+  </body>
+</html>
---
+++ var_constant.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?
+      $a = "자동차";
+      echo "<br>";
+      echo $a;
+
+      $a = "기차";
+      echo "<br>";
+      echo $a;
+
+      $a = 1000;
+      echo "<br>";
+      echo $a;
+    ?>
+  </body>
+</html>
